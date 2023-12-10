7a9b0bc40a79ef11ca3324c685f83c76ce68978a
Martin Soler <martinsoler_91@outlook.com>
2023-12-10 13:16:58 +0100

    delete fragment from Header component

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,20 +1,18 @@
-import { Fragment } from 'react';
-
-import HeaderCartButton from './HeaderCartButton';
-import veggiesImg from '../../assets/veggies.webp';
-import classes from './Header.module.css';
+import HeaderCartButton from "./HeaderCartButton";
+import veggiesImg from "../../assets/veggies.webp";
+import classes from "./Header.module.css";
 
 const Header = (props) => {
   return (
-    <Fragment>
+    <>
       <header className={classes.header}>
         <h1>Green Bowls</h1>
         <HeaderCartButton onClick={props.onShowCart} />
       </header>
-      <div className={classes['main-image']}>
-        <img src={veggiesImg} alt='vegetables' />
+      <div className={classes["main-image"]}>
+        <img src={veggiesImg} alt="vegetables" />
       </div>
-    </Fragment>
+    </>
   );
 };
 
